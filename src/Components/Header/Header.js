2e6759cb74043b2b895b1746344d8140407e71ef
Bharti-Suman <bhartisuman972@gmail.com>
2023-02-14 21:58:53 +0530

    My_First_Project

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from"react"
+import"./Header.css"
+export default function Header(){
+    return(
+        <header className="header">
+            <img src="./trollface.png"  alt="error" className="header--images"></img>
+        <h2 className="header--title">Meme Image</h2>
+        <h4 className="header--project">Project by-Suman Bharti</h4>
+        </header>
+        
+        
+    )
+}
